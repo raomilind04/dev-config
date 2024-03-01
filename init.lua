e19443c5a3ef09d162858d2a9c427fa07466f005
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2024-03-01 17:48:59 +0530

    Update color scheme

--- init.lua
+++ init.lua
@@ -1,16 +1,14 @@
-require("raomil.plugins-setup")
-require("raomil.core.options")
-require("raomil.core.keymaps")
-require("raomil.core.colorscheme")
-require("raomil.plugins.comment")
-require("raomil.plugins.nvim-tree")
-require("raomil.plugins.lualine")
-require("raomil.plugins.telescope")
-require("raomil.plugins.nvim-cmp")
-require("raomil.plugins.lsp.mason")
-require("raomil.plugins.lsp.lspsaga")
-require("raomil.plugins.lsp.lspconfig")
-require("raomil.plugins.lsp.null-ls")
-require("raomil.plugins.autopairs")
-require("raomil.plugins.treesitter")
-require("raomil.plugins.gitsigns")
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.colorscheme"
+require "user.cmp"
+require "user.lsp"
+require "user.telescope"
+require "user.treesitter"
+require "user.autopairs"
+require "user.comments"
+require "user.gitsigns"
+require "user.bufferline"
+require "user.toggleterm"
+require 'nvim-tree'.setup {}
